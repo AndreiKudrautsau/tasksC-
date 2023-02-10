03d387437d7d8aa528b159ef5555c3e2b566df6c
Andrei Kudrautsau <kavalerand@gmail.com>
2023-02-11 00:20:11 +0300

    added task - square yes or no

---
+++ task01/Program.cs
@@ -0,0 +1,17 @@
+﻿int num1, num2;
+
+Console.Write("Введите число #1: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число #2: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2 * num2)
+{
+    Console.Write($"Число {num1} является квадратом числа {num2}");
+}
+
+else
+{
+    Console.Write($"Число {num1} не является квадратом числа {num2}");
+}
